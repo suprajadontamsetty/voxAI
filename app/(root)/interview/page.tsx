56f31e2ad912510243f22365088abedcef163731
Supraja <suprajadb@gmail.com>
2025-03-26 18:12:54 +0530

    implement interview

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Agent from "@/components/Agent";
 
 const page = () => {
   return (
-    <div>Interview Page</div>
+   <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="You" userId="user1" type="generate"/>
+   </>
   )
 }
 
