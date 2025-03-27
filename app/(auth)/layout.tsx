30747d6a6d4d3f22fb47fb6ab1ef111ef7a535a0
Supraja <suprajadb@gmail.com>
2025-03-27 20:21:32 +0530

    Fix auth layer server-side issue

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react';
 import { redirect } from 'next/navigation';
 import { isAuthenticated } from '@/lib/actions/auth.action';
+import { headers } from 'next/headers';
+
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
+  const path = headers().get('next-url') || '';
 
-  // Allow access to sign-in and sign-up pages
-  if (!isUserAuthenticated && !["/auth/sign-in", "/auth/sign-up"].includes(window.location.pathname)) {
-    redirect('/auth/sign-in'); // Redirect to sign-in page instead of looping
-  }
+  if (!isUserAuthenticated && !path.startsWith('/auth/')) {
+    redirect('/auth/sign-in');
+  }  
 
   return <div className="auth-layout">{children}</div>;
 };
