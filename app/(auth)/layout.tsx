724b9e7e740b23273547c38fab5054f4a489fb55
Supraja <suprajadb@gmail.com>
2025-03-27 20:55:02 +0530

    fixed auth layer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,14 @@
-import { ReactNode } from 'react';
+import  { ReactNode } from 'react'
 import { redirect } from 'next/navigation';
 import { isAuthenticated } from '@/lib/actions/auth.action';
-import { headers } from 'next/headers';
 
+const AuthLayout = async({children}:{children: ReactNode}) => {
+   const isUserAuthenticated = await isAuthenticated();
+  
+    if(!isUserAuthenticated) redirect('/');
+  return (
+    <div className='auth-layout'>{children}</div>
+  )
+}
 
-const AuthLayout = async ({ children }: { children: ReactNode }) => {
-  const isUserAuthenticated = await isAuthenticated();
-  const path = headers().get('next-url') || '';
-
-  if (!isUserAuthenticated && !path.startsWith('/auth/')) {
-    redirect('/auth/sign-in');
-  }  
-
-  return <div className="auth-layout">{children}</div>;
-};
-
-export default AuthLayout;
+export default AuthLayout
